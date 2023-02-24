ff9bbe39edc4d5f092b51e5b22215d4cb79ba37d
huppanup <94790989+huppanup@users.noreply.github.com>
2023-02-25 01:02:21 +0800

    Change to explore menu on click

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 function Icon(props) {
       return (
         <svg width="13" height="13" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M9 9.5V34.5L34 22L9 9.5Z" fill="#1A1A1A"/>
+        <path fillRule="evenodd" clipRule="evenodd" d="M9 9.5V34.5L34 22L9 9.5Z" fill="#1A1A1A"/>
         </svg>
       );
   }
@@ -16,11 +16,33 @@ function Icon(props) {
       return (
         <div className='home-bg'>
           <div id="left" className='home-half title'>
-            SAMPLE TITLE
+            <span>Smart</span>
+            <span style={{ fontSize:'80%' }}>Trasnportation</span>
           </div>
           <div id="right" className='home-half description'>
-            <span> Sample description in doughnut div.</span>
-            <button onClick={() => alert("Explore button clicked")}><Icon value="play"/>Explore</button>
+            <span> How does traffic control & large scale simulation innovate network planning?</span>
+            <button onClick={() => this.props.onClick()}><Icon value="play"/>Explore</button>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  class Explore extends React.Component {
+    render() {
+      console.log("EXPLORE")
+      return (
+        <div className='home-bg'>
+          <div id="left" className='home-half'>
+            <span className='title'>DISCO</span>
+            <span className='description'>Dynamic Intersection System Control Optimization</span>
+          </div>
+          <div className='v-divider'></div>
+          <div id="right" className='home-half'>
+            <span className='title'>MATSim</span>
+            <span className='description'>Multi-agent Transport Simulation</span>
+            {/* Button for testing only */}
+            <button onClick={() => this.props.onClick()}><Icon value="play"/>Explore</button>
           </div>
         </div>
       );
@@ -28,11 +50,23 @@ function Icon(props) {
   }
   
   class Doughnut extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        isExplore: false
+      }
+    }
+
+    exploreMenu() {
+      this.setState({isExplore: !this.state.isExplore});
+      document.body.style.boxShadow = !this.state.isExplore? 'inset 0 0 0 100vmax rgba(255, 255, 255, 0.3)': '';
+    }
+
     render() {
       return (
         <div className='doughnut-border'>
         <div className="doughnut">
-            <Home />
+            {this.state.isExplore? <Explore onClick={() => this.exploreMenu()}/>: <Home onClick={() => this.exploreMenu()}/>}
         </div>
         </div>
       );
